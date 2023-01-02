f3a7dbdd4f64362f8dd41abd05c0682b696e27e6
MisterRuby <ruby8700@naver.com>
2023-01-02 21:32:32 +0900

    init : 프로젝트 생성

---
+++ src/main/java/ruby/ooplearn/OopLearnApplication.java
@@ -0,0 +1,13 @@
+package ruby.ooplearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OopLearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ruby/ooplearn/OopLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package ruby.ooplearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
